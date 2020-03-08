33cbb319c5a6e77a0b28f85abcc880fb4ac3eb44
ishwarya17 <58899704+ishwarya17@users.noreply.github.com>
2020-03-08 09:14:38 +0530

    Add files via upload

---
+++ Supplier.java
@@ -0,0 +1,43 @@
+package com.niit.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+
+public class Supplier {
+@Id
+@GeneratedValue
+
+int supplierId;
+String name;
+String description;
+String SupplierName;
+public String getSupplierName() {
+return SupplierName;
+}
+public void setSupplierName(String supplierName) {
+SupplierName = supplierName;
+}
+public int getSupplierId() {
+return supplierId;
+}
+public void setSupplierId(int supplierId) {
+this.supplierId = supplierId;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getDescription() {
+return description;
+}
+public void setDescription(String description) {
+this.description = description;
+}
+
+}
